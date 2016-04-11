3754a9cee5e36a1ec202707921b0fd621bbe5826
Amanda <amandarl@users.noreply.github.com>
2016-04-11 10:10:06 -0600

    Update FedoraPurgeRangeORArray.php

--- Tuque/FedoraPurgeRangeORArray.php
+++ Tuque/FedoraPurgeRangeORArray.php
@@ -12,19 +12,21 @@ $connection = islandora_get_tuque_connection($user);
 $repository = $connection->repository;
 $api_m = $repository->api->m;
 
-//_BREAK__Variables to replace.
+//_BREAK__ Sequence Variables to replace.
 $startPID = '116265';
 $endPID = '116285';
 
-//Iterate through Objects to purge them.
+/* Iterate through Objects to purge them.
+ * Correct the namespace!!
+ */
 for($pid=$startPID; $pid<=$endPID; $pid++) {
         $object = 'islandora:' . $pid;
-		$api_m->purgeObject($object);
+	$api_m->purgeObject($object);
         echo "purged $object \n";
     }
   echo "Purged objects $startPID through $endPID.";
 	
-//_OR__Array
+//_OR__ Array
 $arrPIDS = array(57226, 57242, 57247, 57224, 57230, 57241, 57232); 
 foreach ($arrPIDS as&$pid) {
 	$object = 'wyu:' . $pid;
