1dc5aa494e2799f2874b3ad63a51330f72a0534e
Amanda Lehman <amandarl@users.noreply.github.com>
2017-03-01 14:45:32 -0700

    Add files via upload

--- SSH/IngestSeqPIDs.sh
+++ SSH/IngestSeqPIDs.sh
@@ -1,5 +1,5 @@
-for PID in 80754 42503
+for PID in 50959 140396
 do
 echo "Processing IngestSeqPIDS.sh"
-time drush --root=/var/www/html --user=amandarl --uri=http://localhost.uwyo.edu/ islandora_batch_scan_preprocess --namespace=wyu --content_models=islandora:sp_large_image_cmodel --parent=wyu:$PID --type=directory --target=/home/amandarl/$PID
+time drush --root=/var/www/html --user=amandarl --uri=http://uwdigital.uwyo.edu/ islandora_batch_scan_preprocess --namespace=wyu --content_models=islandora:sp_pdf --parent=wyu:$PID --type=directory --target=/home/amandarl/$PID
 done
